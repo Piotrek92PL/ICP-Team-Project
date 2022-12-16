af65c8eb0c6fce9fe78f8f43e6a08ab5211a8ff5
Piotrek92PL <113793860+Piotrek92PL@users.noreply.github.com>
2022-12-16 10:46:59 +0000

    Deploying to gh-pages from @ Piotrek92PL/ICP-Team-Project@0e11dc3044de42f1b6add7115055b856cfebbefa 🚀

--- src.5b2c4de6.js
+++ src.5b2c4de6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ICP-Team-Project/src.235f15d5.js.map
+//# sourceMappingURL=/ICP-Team-Project/src.5b2c4de6.js.map
